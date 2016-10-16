9d542af5b3318b347c4aa48f5cd90e5a24e55210
ikr_4185 <ikr.4185@gmail.com>
2016-10-17 00:52:58 +0900

    機動部隊抽出スクリプト__結果をcsvっぽく出力する

--- application/cli/CliMtfChecker.php
+++ application/cli/CliMtfChecker.php
@@ -25,9 +25,13 @@ class CliMtfChecker {
 
 		Console::log("Start.");
 
+		// 出力する結果の配列
+		$exports = array();
+		// 行番号
+		$i = 0;
+
 		$search = "機動部隊";
 		$results = $this->ScpreaderLogic->searchScpJpNoLimit( $search );
-
 
 		echo "\nprotocol\n\n";
 
@@ -36,10 +40,18 @@ class CliMtfChecker {
 			preg_match_all("/(機動部隊)(.{1,30})/u",$result["protocol"],$matches);
 			echo $result["item_number"]."\t".$result["title"]."\t";
 
+			$exports[$i] = array(
+				"protocol",
+				$result["item_number"],
+				$result["title"],
+			);
+
 			foreach ($matches[0] as $match) {
 				echo $match."\t";
+				$exports[$i][] = $match;
 			}
 
+			$i++;
 			echo "\n";
 		}
 
@@ -50,12 +62,40 @@ class CliMtfChecker {
 			preg_match_all("/(機動部隊)(.{1,30})/u",$result["description"],$matches);
 			echo $result["item_number"]."\t".$result["title"]."\t";
 
+			$exports[$i] = array(
+				"description",
+				$result["item_number"],
+				$result["title"],
+			);
+
 			foreach ($matches[0] as $match) {
 				echo $match."\t";
+				$exports[$i][] = $match;
+			}
+
+			$i++;
+			echo "\n";
+		}
+
+		// debug //////////////////////////////////////////////////////
+		ob_start();
+
+		foreach ($exports as $line) {
+
+			foreach ( $line as $element ) {
+				echo htmlspecialchars_decode($element).",";
 			}
 
 			echo "\n";
+
 		}
+
+		$ob_data =ob_get_contents();
+		ob_end_clean();
+
+		var_dump ( file_put_contents( "/home/njr-sys/public_html/application/cli/logs/cli_mtf_checker.log", $ob_data ) );
+		// debug //////////////////////////////////////////////////////
+
 
 		Console::log("Done.");
 
