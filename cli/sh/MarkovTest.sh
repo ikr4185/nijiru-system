7c6a2d66f5fe558d6c85ab9a7b5c497e5d21d91f
ikr_4185 <ikr.4185@gmail.com>
2017-03-06 08:33:12 +0900

    mecab__KASHIMAにテスト搭載--絶対パスに変更

--- cli/sh/MarkovTest.sh
+++ cli/sh/MarkovTest.sh
@@ -4,7 +4,7 @@ STRING=$1
 DATE=`date "+%Y%m%d-%H%M%S"`
 TMP_FILE="/home/njr-sys/public_html/test/${DATE}.dat"
 echo ${STRING} > ${TMP_FILE}
-RESULT=`echo ${TMP_FILE} | xargs ~/local/bin/mecab -d ~/local/lib/mecab/dic/ipadic-neologd -b 100000 -Ochasen`
+RESULT=`echo ${TMP_FILE} | xargs /home/njr-sys/local/bin/mecab -d /home/njr-sys/local/lib/mecab/dic/ipadic-neologd -b 100000 -Ochasen`
 rm -rf ${TMP_FILE}
 
 echo "${RESULT}"
