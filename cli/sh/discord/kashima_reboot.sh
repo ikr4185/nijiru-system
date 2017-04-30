ccccfc462fa631220f240f32b05bab1c2a36dcb0
ikr_4185 <ikr.4185@gmail.com>
2017-04-30 20:01:35 +0900

    DiscordLog__リブートのログ記録

--- cli/sh/discord/kashima_reboot.sh
+++ cli/sh/discord/kashima_reboot.sh
@@ -2,14 +2,20 @@
 # rootで実行する事
 # sh /home/njr-sys/public_html/cli/sh/discord/kashima_reboot.sh
 
+# リブートログ
+logfile="/home/njr-sys/public_html/logs/discord/system/reboot.log"
+echo `date +"%Y%m%d_%I%M%S"` >> ${logfile}
+
 #停止
-sh /home/njr-sys/public_html/cli/sh/discord/kashima_stop.sh
+sh /home/njr-sys/public_html/cli/sh/discord/kashima_stop.sh >> ${logfile}
 
 sleep 3s
 
 # 起動
-sh /home/njr-sys/public_html/cli/sh/discord/kashima_start.sh
+sh /home/njr-sys/public_html/cli/sh/discord/kashima_start.sh >> ${logfile}
 
 echo reboot KASHIMA-EXE
+echo ---------------------------------------- >> ${logfile}
+
 
 exit
